384c4ce9a1bea30f432c31dee7de4aa60d4ed9da
Mattias Åslund <mattias.aslund@mantex.se>
2016-05-16 16:24:28 +0200

    SimpleTcpServer.Stop() waits until all underlying listeners have closed their TcpListeners. To do this the TcpListener is wrapped in a TcpListenerEx which only purpose is to publish the protected property "Active".

---
+++ SimpleTCP.Tests/ServerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleTCP.Tests
+{
+	[TestClass]
+	public class ServerTests
+	{
+		[TestMethod]
+		public void Listening_port_opens_and_closes_when_server_starts_and_stops()
+		{
+			var serverPort = 8911;
+			Assert.IsTrue(!IsTcpPortListening(serverPort), "Tcp port should be closed before test starts.");
+
+			var server = new SimpleTcpServer().Start(serverPort);
+			Assert.IsTrue(IsTcpPortListening(serverPort), "Tcp port should be open when server has started.");
+
+			server.Stop();
+			Assert.IsTrue(!IsTcpPortListening(serverPort), "Tcp port should be closed when server has stopped.");
+		}
+
+
+
+		public static bool IsTcpPortListening(int port)
+		{
+			return IPGlobalProperties.GetIPGlobalProperties()
+				.GetActiveTcpListeners()
+				.Where(x => x.Port == port)
+				.Any();
+		}
+	}
+}

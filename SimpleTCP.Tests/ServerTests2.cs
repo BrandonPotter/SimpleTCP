fa7b84739726bd9464cd5b5c8077392b0a86ba5a
Mattias Åslund <mattias@trustfall.se>
2016-05-17 13:32:14 +0200

    Server Start() should throw exception if failed. (#7)
    Now only trying to start listeners on interfaces with OperationalStatus = Up.
    Made it optional to fail if any interface listener failed to start.
    
    Added some debug logging.

---
+++ SimpleTCP.Tests/ServerTests2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleTCP.Tests
+{
+	[TestClass]
+	public class ServerTests2
+	{
+		readonly int _serverPort = 8911;
+
+		[TestMethod]
+		public void Start_passes_if_at_all_nics_passed()
+		{
+			var server = new SimpleTcpServer().Start(_serverPort, false);
+			Assert.IsTrue(server.IsStarted, "Server should have started");
+			server.Stop();
+		}
+
+		[TestMethod]
+		public void Start_passes_if_at_least_one_nic_is_free()
+		{
+			var listener = new System.Net.Sockets.TcpListener(new IPAddress(new byte[] { 127, 0, 0, 1 }), _serverPort);
+			listener.Start();
+			try
+			{
+				var server = new SimpleTcpServer().Start(_serverPort);
+				Assert.IsTrue(server.IsStarted, "Server should have started on free nics");
+				server.Stop();
+			}
+			finally
+			{
+				listener.Stop();
+			}
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void Start_fails_if_at_all_nics_free_is_required()
+		{
+			var listener = new System.Net.Sockets.TcpListener(new IPAddress(new byte[] { 127, 0, 0, 1 }), _serverPort);
+			listener.Start();
+			try
+			{
+				var server = new SimpleTcpServer().Start(_serverPort, false);
+				Assert.IsTrue(server.IsStarted, "Server should have started on free nics");
+				server.Stop();
+			}
+			finally
+			{
+				listener.Stop();
+			}
+		}
+	}
+}

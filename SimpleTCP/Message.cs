22df606bbaad3c078f278397d7763ca6cbc7d679
Fumblemore <rakso97@o2.pl>
2017-11-27 16:06:20 +0100

    Code refactoring

--- SimpleTCP/Message.cs
+++ SimpleTCP/Message.cs
@@ -1,72 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleTCP
 {
     public class Message
     {
-        private TcpClient _tcpClient;
-        private System.Text.Encoding _encoder = null;
-        private byte _writeLineDelimiter;
-        private bool _autoTrim = false;
-        internal Message(byte[] data, TcpClient tcpClient, System.Text.Encoding stringEncoder, byte lineDelimiter)
+        private readonly Encoding _encoder;
+        private readonly byte _writeLineDelimiter;
+        private readonly bool _autoTrim;
+
+        public byte[] Data { get; }
+        public TcpClient TcpClient { get; }
+        public string MessageString => _autoTrim ? _encoder.GetString(Data).Trim() : _encoder.GetString(Data);
+
+
+        internal Message(byte[] data, TcpClient tcpClient, Encoding stringEncoder, byte lineDelimiter)
         {
             Data = data;
-            _tcpClient = tcpClient;
+            TcpClient = tcpClient;
             _encoder = stringEncoder;
             _writeLineDelimiter = lineDelimiter;
         }
 
-        internal Message(byte[] data, TcpClient tcpClient, System.Text.Encoding stringEncoder, byte lineDelimiter, bool autoTrim)
+        internal Message(byte[] data, TcpClient tcpClient, Encoding stringEncoder, byte lineDelimiter, bool autoTrim)
         {
             Data = data;
-            _tcpClient = tcpClient;
+            TcpClient = tcpClient;
             _encoder = stringEncoder;
             _writeLineDelimiter = lineDelimiter;
             _autoTrim = autoTrim;
         }
 
-        public byte[] Data { get; private set; }
-        public string MessageString
-        {
-            get
-            {
-                if (_autoTrim)
-                {
-                    return _encoder.GetString(Data).Trim();
-                }
-
-                return _encoder.GetString(Data);
-            }
-        }
-
         public void Reply(byte[] data)
         {
-            _tcpClient.GetStream().Write(data, 0, data.Length);
+            TcpClient.GetStream().Write(data, 0, data.Length);
         }
 
         public void Reply(string data)
         {
-            if (string.IsNullOrEmpty(data)) { return; }
+            if (string.IsNullOrEmpty(data))
+                return;
+
             Reply(_encoder.GetBytes(data));
         }
 
         public void ReplyLine(string data)
         {
-            if (string.IsNullOrEmpty(data)) { return; }
+            if (string.IsNullOrEmpty(data))
+                return;
+
             if (data.LastOrDefault() != _writeLineDelimiter)
             {
-                Reply(data + _encoder.GetString(new byte[] { _writeLineDelimiter }));
-            } else
-            {
-                Reply(data);
+                Reply(data + _encoder.GetString(new[] { _writeLineDelimiter }));
+                return;
             }
-        }
 
-        public TcpClient TcpClient {  get { return _tcpClient; } }
+            Reply(data);
+        }
     }
 }

84721ad0d9d36555e5b6f2af44ce1546fa74ba4c
Fumblemore <oskark2@o2.pl>
2017-11-29 15:23:16 +0100

    Refactoring

--- SimpleTCP/Server/ConnectedClient.cs
+++ SimpleTCP/Server/ConnectedClient.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SimpleTCP
+namespace SimpleTCP.Server
 {
     public class ConnectedClient
     {
-        public IPAddress ServerIP { get; internal set; }
+        public IPAddress ServerIp { get; internal set; }
         public TcpClient Client { get; internal set; }
     }
 }
